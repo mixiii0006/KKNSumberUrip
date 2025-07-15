d14d08fb63ff5f21dd1fdcf884f21ad80b21cb59
Rana Helida <131280117+Rahel17@users.noreply.github.com>
2025-07-15 19:42:41 +0700

    database

---
+++ app/Models/Organisasi.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Organisasi extends Model
+{
+    protected $fillable = [
+        'nama',
+        'instansi',
+        'jabatan',
+        'nip',
+    ];
+}
---
+++ database/migrations/2025_07_15_110310_create_artikels_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('artikels', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('slug')->unique();
+            $table->longText('isi'); // konten HTML dari CKEditor
+            $table->string('penulis')->default('Admin');
+            $table->date('tanggal_publish');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('artikels');
+    }
+};
